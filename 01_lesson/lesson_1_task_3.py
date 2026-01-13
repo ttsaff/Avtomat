b0a572f4becc2c8a627e4679e83b28d26a340168
Spitful <nosos321@mail.ru>
2026-01-13 19:18:23 +0500

    ДЗ 1

---
+++ 01_lesson/lesson_1_task_3.py
@@ -0,0 +1,8 @@
+#first_name = input()
+#last_name = input()
+#print("Вас зовут:", last_name, first_name)
+
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+
+print(f"Вас зовут: {last_name} {first_name}")
